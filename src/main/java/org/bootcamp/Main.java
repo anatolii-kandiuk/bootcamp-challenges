5be37cb3136ea4b6c8507db0f85d023f41ebb975
anatolii <batkovich327@gmail.com>
2023-02-22 15:37:05 +0200

    Bootcamp - Blackjack v1.0

--- src/main/java/org/bootcamp/Main.java
+++ src/main/java/org/bootcamp/Main.java
@@ -1,5 +1,6 @@
 package org.bootcamp;
 
+import org.bootcamp.Blackjack.Blackjack;
 import org.bootcamp.Hangman.Hangman;
 import org.bootcamp.Pokerito.Pokerito;
 import org.bootcamp.RockPaperScissor.RockPaperScissor;
@@ -7,7 +8,7 @@ import org.bootcamp.RockPaperScissor.RockPaperScissor;
 public class Main {
 
     public static void main(String[] args) {
-        Pokerito.runPokerito();
+        Blackjack.main();
 
     }
 }
