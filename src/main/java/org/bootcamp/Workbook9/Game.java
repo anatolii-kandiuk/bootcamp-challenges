e74751210c8508c4fbd80ee86b59be422bb29b24
anatolii <batkovich327@gmail.com>
2023-02-24 00:52:39 +0200

    Bootcamp - Make Workbook 10 and Update 9

--- src/main/java/org/bootcamp/Workbook9/Game.java
+++ src/main/java/org/bootcamp/Workbook9/Game.java
@@ -3,6 +3,8 @@ package org.bootcamp.Workbook9;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static org.bootcamp.Workbook10.Regulation.*;
+
 public class Game {
 
     private String arena;
@@ -19,6 +21,19 @@ public class Game {
 
         System.out.println(
                 "\n - This matchup takes place at the " + this.arena + " arena on " + formattedDate + "."
+        );
+    }
+
+    public String details() {
+        return (
+                "\nCourt length (feet): " + COURT_LENGTH +
+                        "\nRim height (feet): " + RIM_HEIGHT +
+                        "\nDistance from three-point arc: " + THREE_POINT_DISTANCE +
+                        "\nPoints awarded beyond the three-point arc: " + BEYOND_THREE_POINT_ARC +
+                        "\nPoints awarded inside the three-point arc: " + INSIDE_THREE_POINT_ARC +
+                        "\nPoints awarded from a free throw: " + FREE_THROW +
+                        "\nLength of each quarter (minutes): " + QUARTER_LENGTH +
+                        "\nSeconds to attempt shot after gaining possession: " + POSSESSION_TIME
         );
     }
 }
