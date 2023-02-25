a084fa51add5a2a1d3d8fa14d171ebc9d3c199c7
anatolii <batkovich327@gmail.com>
2023-02-25 19:02:09 +0200

    Bootcamp - Create Account repository

---
+++ src/main/java/org/bootcamp/BankManagement/main/repository/AccountRepository.java
@@ -0,0 +1,27 @@
+package org.bootcamp.BankManagement.main.repository;
+
+import org.bootcamp.BankManagement.main.pojo.Account;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountRepository {
+
+    private Map<String, Account> datastore = new HashMap<>();
+
+    public void createAccount(Account account) {
+
+    }
+
+    public Account retrieveAccount(String id) {
+        return null;
+    }
+
+    public void updateAccount(Account account) {
+
+    }
+
+    public void deleteAccount(String id) {
+
+    }
+}
